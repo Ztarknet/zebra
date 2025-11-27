28391a1bb82d49e49a6972940ce2e609126eb4e8
Maciej Kamiński <aundumla@gmail.com>
2025-11-27 14:53:13 +0000

    Increase MAX_PROTOCOL_MESSAGE_LEN to 2_500_000

--- zebra-chain/src/serialization/zcash_serialize.rs
+++ zebra-chain/src/serialization/zcash_serialize.rs
@@ -6,8 +6,8 @@ use super::{AtLeastOne, CompactSizeMessage};
 
 /// The maximum length of a Zcash message, in bytes.
 ///
-/// This value is used to calculate safe preallocation limits for some types
-pub const MAX_PROTOCOL_MESSAGE_LEN: usize = 2 * 1024 * 1024;
+/// This value is used to calculate safe preallocation limits for some types.
+pub const MAX_PROTOCOL_MESSAGE_LEN: usize = 2_500_000;
 
 /// Consensus-critical serialization for Zcash.
 ///
